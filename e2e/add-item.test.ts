c8b179fc14af121fdfd25fb8392134a6e7f56b07
Silvan Schell <silvan.schell@edu.gbssg.ch>
2025-03-05 10:20:15 +0100

    feat: tests

---
+++ e2e/add-item.test.ts
@@ -0,0 +1,9 @@
+import { expect, test } from '@playwright/test';
+
+test('new item is added', async ({ page }) => {
+    const itemText = "test item";
+    await page.goto('/');
+    await page.locator('input').fill(itemText);
+    await page.getByRole('button', { name: 'Add' }).click();
+    await expect(page.getByRole('listitem')).toContainText(itemText);
+});
